5a7640663395a9c78cefcd9dad5260288fb755f1
Link <xlinkx_@outlook.com>
2021-05-20 13:26:19 +0100

    date not working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,4 @@
 from flask import render_template, request
-from flask.templating import render_template
 from app import app
 from models.event_planner import events, add_new_event
 from models.event import Event
@@ -14,12 +13,13 @@ def events_index():
 
 @app.route('/events', methods=['POST'])
 def add_event():
+    event_date = request.form['date']
     event_name = request.form['name']
     event_num_of_guests = request.form['num_of_guests']
     event_room = request.form['room']
     event_description = request.form['description']
     # Create a new event object with the events posted from form
-    new_event = Event(event_name, event_num_of_guests, event_room, event_description)
+    new_event = Event(event_date, event_name, event_num_of_guests, event_room, event_description)
     # Append event to the list (you could do this directly here, but we put it in the event_planner file)
     add_new_event(new_event)
     return render_template('index.html', title='Home', my_events=events)
--- models/event.py
+++ models/event.py
@@ -1,6 +1,8 @@
 class Event():
 
-    def __init__(self, name, num_of_guests, room, description):
+    def __init__(self, date, name, num_of_guests, room, description):
+        
+        self.date = date
         self.name = name
         self.num_of_guests = num_of_guests
         self.room = room
