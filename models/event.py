3f3fdd87a0019b621ad2b965b7df751fb91cf222
Link <xlinkx_@outlook.com>
2021-05-20 13:13:03 +0100

    mvp

--- models/event.py
+++ models/event.py
@@ -1,8 +1,8 @@
 class Event():
 
-    def __init__(self, name, num_of_guests, room, description, recurring):
+    def __init__(self, name, num_of_guests, room, description):
         self.name = name
         self.num_of_guests = num_of_guests
         self.room = room
         self.description = description
-        self.recurring = recurring
+        
---
+++ models/event_planner.py
@@ -0,0 +1,10 @@
+from models.event import *
+
+event_1 = Event("Ceilidh", 50, 1, "St Andrews Day event")
+event_2 = Event("Gardening Club Meeting", 10, 14, "meeting to discuss new town project")
+events = [event_1, event_2]
+
+def add_new_event(event):
+    events.append(event)
+
+
